25afb517e2d0371f495fa4c8c58d7a0e6c5ef574
Divyanshi <vishnoidivyanshi122@gmail.com>
2025-02-13 00:27:10 +0530

    added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
 const express=require("express");
 const app=express();
+const PORT=5000;
 
 app.get("",(req,res)=>{
     res.json("hey");
 })
 
-app.listen(prompt,()=>{
+app.listen(PORT,()=>{
     console.log(`server is running on the ${PORT}`);
 });
